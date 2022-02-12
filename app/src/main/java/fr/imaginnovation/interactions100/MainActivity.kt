ae84e8e10ae0a4cfdc8e37f1be9c0c2d859aca12
N03M0TT3 <noemotte@gmail.com>
2022-02-12 17:34:30 +0100

    Transformed number of interactions as a SharedPreference to store it in the phone

--- app/src/main/java/fr/imaginnovation/interactions100/MainActivity.kt
+++ app/src/main/java/fr/imaginnovation/interactions100/MainActivity.kt
@@ -1,21 +1,33 @@
 package fr.imaginnovation.interactions100
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val sharedPreferences = getPreferences(MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        var interactionNumber = sharedPreferences.getInt("interactions", -1)
+        if(interactionNumber == -1) {
+            editor.putInt("interactions", 0)
+            editor.commit()
+        }
+
         val textNumberInteraction = findViewById<TextView>(R.id.numberInteractionText)
         val buttonNewInteraction = findViewById<Button>(R.id.newInteractionButton)
         val buttonReset = findViewById<Button>(R.id.resetInteractionButton)
-        var interactionNumber = 0
+
+        textNumberInteraction.text = "Interactions : $interactionNumber"
 
         buttonNewInteraction.setOnClickListener {
+            var interactionNumber = sharedPreferences.getInt("interactions", -1)
             if(interactionNumber >= 100) {
                 interactionNumber = 0
             } else {
@@ -23,11 +35,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             textNumberInteraction.text = "Interactions : $interactionNumber"
+            editor.putInt("interactions", interactionNumber)
+            editor.commit()
         }
 
         buttonReset.setOnClickListener {
-            interactionNumber = 0
-            textNumberInteraction.text = "Interactions : $interactionNumber"
+            editor.putInt("interactions", 0)
+            editor.commit()
+            textNumberInteraction.text = "Interactions : 0"
         }
 
     }
